7078a9fee724508d9c8b5535a78ac5c45fa04447
Pierce Heyerdahl <pierce.heyerdahl@bellevuecollege.edu>
2021-06-15 16:17:58 -0700

    Add test cases

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -18,14 +18,32 @@ public class CILabTest {
         myString = null;
     }
 
+    // test for all upper case
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+        myString.setString("ALLUPPERCASE");
+        assertTrue(myString.detectCapitalUse());
     }
+
+    // test for all lower case
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("alllowercase");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    // test for capital letters at start and end
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("FirstandlastcapitaL");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    // test for capital letter in middle
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("capitalInmiddle");
+        assertFalse(myString.detectCapitalUse());
     }
 
 
